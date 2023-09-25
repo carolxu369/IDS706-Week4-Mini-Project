9839dd85c20154b82421e1e599524d07e309323e
carolxu <67770063+carolxu369@users.noreply.github.com>
2023-09-24 23:11:21 -0400

    update test

--- test_main.py
+++ test_main.py
@@ -1,13 +1,14 @@
 from main import calculate
 
+import pytest
+
+@pytest.fixture
+def data():
+    return [15, 23, 43, 11, 5, 30, 8, 19, 7, 50, 28, 37, 14, 21, 9, 63, 31, 2, 45, 12]
+
 def test(data):
     mean, median, std_dev = calculate(data)
 
     assert mean == 23.65
     assert median == 20.0
     assert std_dev == 16.875004873293644
-
-if __name__ == "__main__":
-    data = [15, 23, 43, 11, 5, 30, 8, 19, 7, 50, 28, 37, 14, 21, 9, 63, 31, 2, 45, 12]
-    test(data)
-
